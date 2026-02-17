fa87a11487d158c5eed5a4598579a21fed42d220
Manodya Paramee <manodyaparamee20@gmail.com>
2026-02-17 22:39:47 +0530

    Add Question model, Role enum, and UserRepository interface

---
+++ src/main/java/com/system/mcqexam/model/Question.java
@@ -0,0 +1,20 @@
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String questionText;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+    private String optionD;
+    private String correctAnswer;
+}
