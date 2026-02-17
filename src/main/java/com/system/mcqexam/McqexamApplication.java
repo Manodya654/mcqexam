44a370e9eb58968d51101503c5b140ed128d8d14
Manodya Paramee <manodyaparamee20@gmail.com>
2026-02-17 21:59:23 +0530

    initial commit

---
+++ src/main/java/com/system/mcqexam/McqexamApplication.java
@@ -0,0 +1,13 @@
+package com.system.mcqexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McqexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McqexamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/mcqexam/McqexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.mcqexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McqexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
